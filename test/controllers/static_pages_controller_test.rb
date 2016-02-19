61d0f4629e04bc856b0920b2c4ef1b748278bdc2
Noor <noorel-sakka@hotmail.com>
2016-02-19 17:14:51 +0300

    fixed the error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get home" do
-    root :home
+    get :home
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
